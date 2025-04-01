150df49df4b14199a5bf63347e3bcbc459e49a97
Yuri Lucatelli Taba <yuri.l.taba@gmail.com>
2025-04-01 19:04:42 -0300

    teste odbc fabric

--- pages/des_urbano.py
+++ pages/des_urbano.py
@@ -3,6 +3,39 @@ import dash_bootstrap_components as dbc
 from src.utils import create_info_popover
 
 register_page(__name__, path="/desenvolvimento_urbano", name="Desenvolvimento Urbano")
+import pyodbc
+import pandas as pd
+import plotly.express as px
+from dash import Dash, dcc, html
+
+# data
+server = 'ena6obg6j2cevcppw7dn7yu57a-ft5kzcrrso7exn77ioi727kmza.datawarehouse.fabric.microsoft.com'
+database = 'cidade_inteligente'
+
+conn = pyodbc.connect(
+    f"Driver={{ODBC Driver 18 for SQL Server}};"
+    f"Server={server};"
+    f"Database={database};"
+    "Authentication=ActiveDirectoryInteractive"
+)
+
+query = """
+SELECT TOP (10) [ano],
+            [sigla_uf],
+            [id_municipio],
+            [quantidade_vinculos_ativos],
+            [quantidade_vinculos_clt],
+            [quantidade_vinculos_estatutarios],
+            [tamanho_estabelecimento],
+            [cnae_1]
+FROM [cidade_inteligente].[dbo].[rais_estab]
+"""
+
+df = pd.read_sql(query, conn)
+
+fig = px.histogram(df, x="quantidade_vinculos_ativos", nbins=20)
+
+
 
 layout = html.Div(
     [
@@ -63,6 +96,7 @@ layout = html.Div(
             ],
             className="section-container",
             style={"marginBottom": "3rem"},
-        )
+        ),
+        dcc.Graph(figure=fig),
     ]
 )

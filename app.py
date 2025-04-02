6657700e9dddb17940246712fdd220101089f9b3
Yuri Lucatelli Taba <yuri.l.taba@gmail.com>
2025-04-02 12:03:11 -0300

    inclusao senha acesso

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output, State
 import dash_bootstrap_components as dbc
 import dash
 
@@ -6,6 +6,7 @@ from src.config import DATA_PATH
 from src.load_data import load_data
 from src.callbacks import init_callbacks
 
+VALID_PASSWORD = "Oz1962"
 
 external_stylesheets = [
     dbc.themes.SANDSTONE,
@@ -23,7 +24,6 @@ app.title = "Painel de Governo PMO"
 data_path = DATA_PATH
 all_data = load_data()
 
-
 # Logo Osasco
 imagem_cabecalho = html.Img(
     src="https://osasco.sp.gov.br/wp-content/uploads/2024/12/logo-pmo-2025-2028-horizontal.png",
@@ -36,21 +36,41 @@ imagem_cabecalho = html.Img(
     },
 )
 
-navbar = dbc.Nav(
+# Login layout
+login_layout = dbc.Container(
     [
-        dbc.NavLink(
-            [html.Div(page["name"])],
-            href=page["path"],
-            active="exact",
-        )
-        for page in dash.page_registry.values()
+        imagem_cabecalho,
+        html.Br(),
+        dbc.Row(
+            dbc.Col(
+                dbc.Card(
+                    dbc.CardBody([
+                        html.H4("Login", className="text-center"),
+                        html.Br(),
+                        dbc.Input(
+                            type="password",
+                            id="password",
+                            placeholder="Digite a senha",
+                            className="mb-3",
+                        ),
+                        dbc.Button(
+                            "Entrar",
+                            id="login-button",
+                            color="primary",
+                            className="w-100",
+                        ),
+                        html.Div(id="login-error", className="text-danger mt-3"),
+                    ]),
+                ),
+                width={"size": 6, "offset": 3},
+            ),
+        ),
     ],
-    pills=True,
-    className="mb-3",
+    className="mt-5",
 )
 
-
-app.layout = dbc.Container(
+# Main app layout
+main_layout = dbc.Container(
     [
         imagem_cabecalho,
         html.Br(),
@@ -58,6 +78,37 @@ app.layout = dbc.Container(
     ]
 )
 
+# Layout + autenticação
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    dcc.Store(id='authenticated', storage_type='session'),
+    html.Div(id='page-content')
+])
+
+# Callback de autenticação
+@app.callback(
+    [Output('authenticated', 'data'),
+     Output('login-error', 'children')],
+    [Input('login-button', 'n_clicks')],
+    [State('password', 'value')],
+    prevent_initial_call=True
+)
+def authenticate(n_clicks, password):
+    if not n_clicks:
+        return False, ''
+    if password == VALID_PASSWORD:
+        return True, ''
+    return False, 'Senha incorreta'
+
+# Callback de conteúdo da página
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('authenticated', 'data')]
+)
+def display_page(authenticated):
+    if not authenticated:
+        return login_layout
+    return main_layout
 
 init_callbacks(app, all_data)
 
